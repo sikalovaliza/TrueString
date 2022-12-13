0a4f47d4504d36fb62f67d005d4256c662eba705
sikalovaliza <116655998+sikalovaliza@users.noreply.github.com>
2022-12-13 13:37:40 +0300

    #4 тесты не проходит

--- string.h
+++ string.h
@@ -4,21 +4,18 @@
 
 class String {
  public:
-  String(size_t size, char element = '\0') {
-    size_of_string_ = size;
-    capacity_ = 2 * size_of_string_;
-    std::free(array_of_string_);
+  explicit String(size_t size, char element = '\0')
+      : size_of_string_{size}, capacity_{size + 1} {
     array_of_string_ =
         static_cast<char*>(std::malloc(capacity_ * sizeof(char)));
     memset(array_of_string_, element, size_of_string_);
     array_of_string_[size_of_string_] = '\0';
   }
 
-  String(const char* new_string) {
+  String(const char* new_string) : size_of_string_(0), capacity_(0) {
     if (new_string != nullptr) {
       size_t count = strlen(new_string);
-      capacity_ = 1 + int(count << 1);
-      std::free(array_of_string_);
+      capacity_ = count + 1;
       array_of_string_ =
           static_cast<char*>(std::malloc(capacity_ * sizeof(char)));
       memcpy(array_of_string_, new_string, count);
@@ -27,17 +24,29 @@ class String {
     }
   }
 
-  String() = default;
+  String() {
+    capacity_ = 1;
+    array_of_string_ =
+        static_cast<char*>(std::malloc(capacity_ * sizeof(char)));
+    array_of_string_[0] = '\0';
+  }
 
   String(const String& new_string) : String(new_string.data()) {}
 
   String(const char kElement) : String(1, kElement) {}
-  String& operator+=(const String& right_string) {
-    while (size_of_string_ + right_string.size_of_string_ >= capacity_) {
-      capacity_ = 2 * capacity_ + 1;
+  
+  String& reserve(const String& new_string) {
+    if (capacity_ < size_of_string_ + new_string.size_of_string_) {
+      capacity_ = std::max(2 * capacity_ + 1,
+                           size_of_string_ + new_string.size_of_string_);
       array_of_string_ = static_cast<char*>(
           std::realloc(array_of_string_, capacity_ * sizeof(char)));
     }
+    return *this;
+  }
+  
+  String& operator+=(const String& right_string) {
+    reserve(right_string);
     memcpy(array_of_string_ + size_of_string_, right_string.array_of_string_,
            right_string.size_of_string_);
     size_of_string_ += right_string.size_of_string_;
@@ -45,45 +54,24 @@ class String {
     return *this;
   }
 
-  String& operator=(const String& string_new) {
-    if (string_new.size_of_string_ == 0) {
-      std::free(array_of_string_);
-      array_of_string_ = nullptr;
-      return *this;
-    }
-    if (capacity_ < string_new.capacity_) {
-      capacity_ = string_new.capacity_ + 1;
-      array_of_string_ = static_cast<char*>(
-          std::realloc(array_of_string_, capacity_ * sizeof(char)));
-    }
-    size_of_string_ = string_new.size_of_string_;
-    memcpy(array_of_string_, string_new.array_of_string_, size_of_string_);
+  String& swap(String& string_new) {
+    std::swap(size_of_string_, string_new.size_of_string_);
+    std::swap(array_of_string_, string_new.array_of_string_);
+    std::swap(capacity_, string_new.capacity_);
     return *this;
   }
-
-  char& operator[](size_t index) {
-    if (index < size_of_string_) {
-      return array_of_string_[index];
-    }
-    static char value = '\0';
-    return value;
+  
+  String& operator=(const String& right_string) {
+    String new_string{right_string};
+    return swap(new_string);
   }
+  const char& operator[](size_t index) const { return array_of_string_[index]; }
 
-  const char& operator[](size_t index) const {
-    if (index < size_of_string_) {
-      return array_of_string_[index];
-    }
-    static char value = '\0';
-    return value;
-  }
+  char& operator[](size_t index) { return array_of_string_[index]; }
 
   String substr(size_t start, size_t count) const {
     if (start + count < size_of_string_ + 1) {
-      String new_string;
-      new_string.capacity_ = 2 * count + 1;
-      new_string.array_of_string_ =
-          static_cast<char*>(std::malloc(new_string.capacity_ * sizeof(char)));
-      new_string.size_of_string_ = count;
+      String new_string(count);
       memcpy(new_string.array_of_string_, array_of_string_ + start, count);
       new_string[size_of_string_] = '\0';
       return new_string;
@@ -92,58 +80,15 @@ class String {
   }
 
   size_t find(const String kSubstring) const {
-    size_t count = 0;
-    size_t index_substring = 0;
-    size_t index = 0;
-    while (index < size_of_string_) {
-      if (array_of_string_[index] == kSubstring[index_substring]) {
-        ++count;
-        ++index_substring;
-        if (count == kSubstring.size()) {
-          return index - count + 1;
-        }
-      } else {
-        index -= count;
-        count = 0;
-        index_substring = 0;
-      }
-      ++index;
-    }
-    return size_of_string_;
+    return universalfind(true, kSubstring);
   }
 
   size_t rfind(const String kSubstring) const {
-    size_t count = 0;
-    size_t index_substring = kSubstring.size_of_string_ - 1;
-    size_t index = size_of_string_ - 1;
-    while (index < size_of_string_) {
-      if (array_of_string_[index] == kSubstring[index_substring]) {
-        ++count;
-        --index_substring;
-        if (count == kSubstring.size()) {
-          return index;
-        }
-      } else {
-        index += count;
-        count = 0;
-        index_substring = kSubstring.size_of_string_ - 1;
-      }
-      --index;
-    }
-    return size_of_string_;
+    return universalfind(false, kSubstring);
   }
 
   void push_back(char element) {
-    if (capacity_ == 0) {
-      capacity_ = 2;
-      array_of_string_ = static_cast<char*>(
-          realloc(array_of_string_, capacity_ * sizeof(char)));
-    }
-    if (size_of_string_ + 1 >= capacity_) {
-      capacity_ <<= 1;
-      array_of_string_ = static_cast<char*>(
-          realloc(array_of_string_, capacity_ * sizeof(char)));
-    }
+    reserve(element);
     array_of_string_[size_of_string_] = element;
     array_of_string_[size_of_string_ + 1] = '\0';
     ++size_of_string_;
@@ -161,8 +106,8 @@ class String {
 
   void shrink_to_fit() {
     capacity_ = size_of_string_ + 1;
-    array_of_string_ = static_cast<char*>(
-        realloc(array_of_string_, size_of_string_ * sizeof(char)));
+    array_of_string_ =
+        static_cast<char*>(realloc(array_of_string_, capacity_ * sizeof(char)));
   }
 
   size_t length() const { return size_of_string_; }
@@ -195,6 +140,39 @@ class String {
   size_t size_of_string_ = 0;
   size_t capacity_ = 0;
   char* array_of_string_ = nullptr;
+  
+  size_t static change(bool first, size_t value_first, size_t value_second) {
+    if (first) {
+      return value_first;
+    }
+    return value_second;
+  }
+  
+  size_t universalfind(bool first, const String kSubstring) const {
+    size_t count = 0;
+    size_t index_substring = 0;
+    size_t index = 0;
+    if (!first) {
+      index_substring = change(first, 0, kSubstring.size_of_string_ - 1);
+      index = change(first, 0, size_of_string_ - 1);
+    }
+    while (index < size_of_string_) {
+      if (array_of_string_[index] == kSubstring[index_substring]) {
+        ++count;
+        index_substring =
+            change(first, index_substring + 1, index_substring - 1);
+        if (count == kSubstring.size()) {
+          return change(first, index - count + 1, index);
+        }
+      } else {
+        index = change(first, index - count, index + count);
+        index_substring = change(first, 0, kSubstring.size_of_string_ - 1);
+        count = 0;
+      }
+      index = change(first, index + 1, index - 1);
+    }
+    return size_of_string_;
+  }
 };
 
 String operator+(const String& left_string, const String& right_string) {
@@ -205,46 +183,22 @@ String operator+(const String& left_string, const String& right_string) {
 bool operator<(const String& left_string, const String& right_string) {
   for (size_t index = 0;
        index < std::max(left_string.size(), right_string.size()); ++index) {
-    if (right_string[index] < left_string[index]) {
-      return false;
-    }
-    if (left_string[index] < right_string[index]) {
-      return true;
+    if (left_string[index] != right_string[index]) {
+      return left_string[index] < right_string[index];
     }
   }
   return false;
 }
 
 bool operator>(const String& left_string, const String& right_string) {
-  for (size_t index = 0;
-       index < std::max(left_string.size(), right_string.size()); ++index) {
-    if (right_string[index] < left_string[index]) {
-      return true;
-    }
-    if (left_string[index] < right_string[index]) {
-      return false;
-    }
-  }
-  return false;
+  return right_string < left_string;
 }
 
 bool operator<=(const String& left_string, const String& right_string) {
-  for (size_t index = 0;
-       index < std::max(left_string.size(), right_string.size()); ++index) {
-    if (right_string[index] < left_string[index]) {
-      return false;
-    }
-  }
-  return true;
+  return !(right_string < left_string);
 }
 bool operator>=(const String& left_string, const String& right_string) {
-  for (size_t index = 0;
-       index < std::max(left_string.size(), right_string.size()); ++index) {
-    if (left_string[index] < right_string[index]) {
-      return false;
-    }
-  }
-  return true;
+  return !(left_string < right_string);
 }
 
 bool operator==(const String& left_string, const String& right_string) {
@@ -260,15 +214,7 @@ bool operator==(const String& left_string, const String& right_string) {
 }
 
 bool operator!=(const String& left_string, const String& right_string) {
-  if (left_string.size() != right_string.size()) {
-    return true;
-  }
-  for (size_t index = 0; index < left_string.size(); ++index) {
-    if (left_string[index] != right_string[index]) {
-      return true;
-    }
-  }
-  return false;
+  return !(left_string == right_string);
 }
 
 std::ostream& operator<<(std::ostream& output, const String& new_string) {
